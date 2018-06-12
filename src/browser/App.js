904c30cd0b14cb2349eb677e852e5e5dde9f8a06
Mahad Walusimbi <engmk123@gmail.com>
2018-06-12 19:21:04 +0300

    add reducer structure and store

--- src/browser/App.js
+++ src/browser/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -7,9 +8,14 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import store from './store';
+
+
+
 class App extends Component {
     render() {
         return (
+            <Provider store={store}>
             <Router>
                 <div className='App'>
                     <Navbar />
@@ -21,6 +27,7 @@ class App extends Component {
                     <Footer />
                 </div>
             </Router>
+            </Provider>
         )
     }
 }
---
+++ src/browser/reducers/auth-reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    isAuthenticated: false,
+    user: {},
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        // case SET_CURRENT_USER:
+        //     break;
+        default:
+            return state;
+    }
+}
